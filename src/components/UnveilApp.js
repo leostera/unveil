7034ab21ebeb685b38f5b65da9e3d4bdd2142c4e
Leandro Ostera <leandro@ostera.io>
2016-01-16 00:29:58 +0100

    Refactors routing

--- src/components/UnveilApp.js
+++ src/components/UnveilApp.js
@@ -6,6 +6,19 @@ import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 import history from '../helpers/History';
 
+// Please kill me with fire
+Object.prototype.toList = function () {
+  return Array.isArray(this) && this || [this];
+};
+
+Array.prototype.flatten = function () {
+  return this.reduce((a,b) => (a.concat(b)), []);
+};
+
+Array.prototype.compact = function () {
+  return this.filter((a) => (a !== null && a !== undefined));
+};
+
 export default React.createClass({
 
   cleanUpPath: path => {
@@ -24,40 +37,64 @@ export default React.createClass({
       .map(this.cleanUpPath)
       .filter(this.emptyPath)
       .distinctUntilChanged()
-      .map(this.pathToRoute)
-      .map(this.lookupRoute)
+      .map(this.toKeypair)
+      .map(this.toIndices)
+      .map(this.toSlide)
       .subscribe(this.route);
   },
 
-  pathToRoute: (path) => {
-    const [key, subKey] = path.split("/");
-    return {key, subKey};
+  /**
+   * in:  "hello/world"
+   * out: ["hello", "world"]
+   */
+  toKeypair: (path) => (path.split("/")),
+
+  /**
+   * in:  ["hello", "world"]
+   * out: [1, 3]
+   */
+  toIndices: function (keypair) {
+    let list = this.props.children.toList();
+    let [first, children] = this.pathToIndex(keypair[0], list);
+    let second;
+    if(typeof children !== "string")
+      [second] = this.pathToIndex(keypair[1], children.toList());
+    return [first, second].compact();
   },
 
-  lookupRoute: function (route) {
-    let byKey = (key) => {
-      return (slide) => (slide.key === key)
+  /*
+   * Looks up a slide in a list of slides either by
+   * index or by named key
+   */
+  pathToIndex: (key, list) => {
+    let n = Number(key);
+    let getChildren = (element) => {
+      return element.props.children;
     };
-    let toChildren = (slide) => (slide.props.children);
-    let children = this.props.children;
-    if (!Array.isArray(children)) {
-      children = [children];
-    }
-    children = children.filter(byKey(route.key))
-      .map(toChildren)
-      .reduce( (a,b) => (a.concat(b)), [] );
 
-    if (route.subKey) {
-      return children.filter(byKey(route.subKey));
-    } else if (typeof children === "String") {
-      return children;
-    } else {
-      return children[0];
-    }
+    let byKey = (el, index) => {
+      return el.name===key && [index, getChildren(el)];
+    };
+
+    if(Number.isInteger(n))
+      return [n, getChildren(list[n])];
+    else
+      return list.map(byKey).flatten().compact();
   },
 
-  route: function (element) {
-    this.setState({current: element});
+  toSlide: function (indeces) {
+    let slide = this.props.children.toList()[indeces[0]];
+    if(indeces.length > 1) {
+      let children = slide.props.children;
+      if(typeof children !== "string") {
+        slide = children.toList()[indeces[1]];
+      }
+    }
+    return slide;
+  },
+
+  route: function (current) {
+    this.setState({current});
   },
 
   render: function () {
