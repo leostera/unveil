1e2f7c025f93abedd5016224f89a5c6f69ffe03e
Leandro Ostera <leandro@ostera.io>
2016-01-21 04:13:58 +0100

    Reformatting util tests

--- src/lib/__tests__/Utils.js
+++ src/lib/__tests__/Utils.js
@@ -50,14 +50,12 @@ describe("Utils", () => {
     it("flattens an array", () => {
       const a = [1,[2,3]];
       const b = [1,2,3];
-
       expect(a.flatten()).toEqual(b);
     });
 
     it("flattens an array recursively", () => {
       const a = [1,[2,[3]]];
       const b = [1,2,3];
-
       expect(a.flatten()).toEqual(b);
     });
 
@@ -66,30 +64,26 @@ describe("Utils", () => {
   describe("Array.prototype.equals", () => {
 
     it("fails on different length arrays", () => {
-      const a = [1,2],
-            b = [1,2,3];
-
+      const a = [1,2];
+      const b = [1,2,3];
       expect(a.equals(b)).toEqual(false);
     });
 
     it("succeeds when arrays have equal length and elements", () => {
-      const a = [1,2,3],
-            b = [1,2,3];
-
+      const a = [1,2,3];
+      const b = [1,2,3];
       expect(a.equals(b)).toEqual(true);
     });
 
     it("fails when arrays have equal length but different elements", () => {
-      const a = [1,2,3],
-            b = [1,2,5];
-
+      const a = [1,2,3];
+      const b = [1,2,5];
       expect(a.equals(b)).toEqual(false);
     });
 
     it("fails when arrays have equal length but differently positioned elements", () => {
-      const a = [1,2,3],
-            b = [1,3,2];
-
+      const a = [1,2,3];
+      const b = [1,3,2];
       expect(a.equals(b)).toEqual(false);
     });
 
