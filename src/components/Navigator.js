d487e748c7cfb0a59c31bcfb1942370af21543d6
Leandro Ostera <leandro@ostera.io>
2016-01-21 19:12:34 +0100

    Cleans up the Navigator

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,26 +1,25 @@
-let createNavigator = (map) => {
-  // @todo add Observer that updates map here!
-
+let createNavigator = () => {
   /**
    * Returns possible directions from state on each level.
    * If we are at state [1, 0] depending on the map, the
    * result might look something like this:
    * [
-   *    0: {
+   *    {
    *      next: [2],
    *      previous: [0]
    *    },
-   *    1: {
+   *    {
    *      next: [1, 1],
    *      previous: false
    *    }
    * ]
    *
    * @param {number[]} state State for which to return the directions
-   * @param {*[]} elements Elements to go through. Defaults to map.
-   * @param {number} level Depth of recursion. Defaults to 0.
+   * @param {*[]} elements Elements to go through
+   * @param {number} level Depth of recursion. Defaults to 0
+   * @return {[directions]} List of direcitons object
    */
-  let getDirections = (state, elements = map, level = 0) => {
+  let getDirections = (state, elements, level = 0) => {
     if (level  === state.length) return [];
 
     let index = state[level];
@@ -36,9 +35,9 @@ let createNavigator = (map) => {
    * @param {*[]} elements Elements to look into
    * @param {number} index index of state at this level
    * @param {number[]} stateLevel state until level
-   * @returns {{next, previous}} object with indices
-   *                             of next and previous
-   *                             indices. See @getIndex
+   *
+   * @returns {{next, previous}} object with indices of
+   * next and previous indices. See @getIndex
    */
   let getDirectionObject = (elements, index, stateLevel) => {
     return {
@@ -54,7 +53,7 @@ let createNavigator = (map) => {
    * @param {*[]} elements
    * @param {number} index
    * @param {number[]} stateLevel state
-   * @returns {boolean|number[]} state array or false
+   * @returns {boolean|number[]} state list or false
    */
   let getIndex = (elements, index, stateLevel) => {
     return elements[index] !== undefined && stateLevel.concat([index]) || false;
