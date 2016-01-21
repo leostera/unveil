7fa72d70649201eef50f731e2c1f7d471049272c
Leandro Ostera <leandro@ostera.io>
2016-01-21 03:17:10 +0100

    Sets starting value to true on equality reduction

--- src/lib/Utils.js
+++ src/lib/Utils.js
@@ -21,5 +21,5 @@ Array.prototype.equals = function (other) {
   return this.length === other.length
     && this
       .map((a, i) => (other[i] === a))
-      .reduce((a, b) => (a && b));
+      .reduce((a, b) => (a && b), true);
 };
