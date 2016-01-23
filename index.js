ddeb25c10f8b4b9d6701a931c39ff19743c3e98b
Leandro Ostera <leandro@ostera.io>
2016-01-23 22:23:33 +0100

    Adds index to be importable!

---
+++ index.js
@@ -0,0 +1,14 @@
+import UnveilApp from './components/UnveilApp';
+import Slide     from './components/Slide';
+
+import KeyControls from './components/KeyControls';
+import UIControls  from './components/UIControls';
+
+const Unveil = {
+  UnveilApp,
+  Slide,
+  KeyControls,
+  UIControls
+};
+
+export default Unveil;
