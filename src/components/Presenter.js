4a88a37683d669a15e4f70181fd532715c72c819
Iris Schaffer <irisSchaffer@gmx.net>
2016-01-25 22:21:08 +0100

    Fixes bug in getSlide

--- src/components/Presenter.js
+++ src/components/Presenter.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default React.createClass({
 
   getSlide: function (indices) {
-    let slide = this.props.slides[indices[0]];
+    let slide = this.props.slides.toList()[indices[0]];
     if(indices.length > 1 )
-      return slide.props.children[indices[1]];
+      return slide.props.children.toList()[indices[1]];
     else
       return slide
   },
