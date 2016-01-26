d00e359370c335f4b596c67357c13d5e42619873
Leandro Ostera <leandro@ostera.io>
2016-01-26 06:52:49 +0100

    Adds main export file

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import History         from './helpers/History';
+import KeyControls     from './components/KeyControls';
+import Presenter       from './components/Presenter';
+import Slide           from './components/Slide';
+import UIControls      from './components/UIControls';
+import UnveilApp       from './components/UnveilApp';
+import createNavigator from './components/Navigator';
+import createRouter    from './components/Router';
+
+const Unveil = {
+  History,
+  KeyControls,
+  Presenter,
+  Slide,
+  UIControls,
+  UnveilApp,
+  createNavigator,
+  createRouter,
+};
+
+export default Unveil;
