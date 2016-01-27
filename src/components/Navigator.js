03161110c9060302f7c0fc6bc2c685c60148563c
Leandro Ostera <leandro@ostera.io>
2016-01-27 22:34:03 +0100

    Fix rebase conflict.

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -5,32 +5,18 @@ let createNavigator = (opts) => {
 
   let subject = new Subject();
 
-<<<<<<< HEAD
   let possibleMotions = [];
   let directions = {};
-=======
-  let possibleMoves = [];
-  let possibleDirections = {};
->>>>>>> Extracts MoveCalculator and refactors Navigator
 
   /**
    * Maps motion-names to position in directions array
    *
-<<<<<<< HEAD
    * @type {[
    *  left:  {level: number, direction: string},
    *  up:    {level: number, direction: string},
    *  right: {level: number, direction: string},
    *  down:  {level: number, direction: string}
    * ]}
-=======
-   * @type {{
-   *  left: {level: number, direction: string},
-   *  up: {level: number, direction: string},
-   *  right: {level: number, direction: string},
-   *  down: {level: number, direction: string}
-   * }}
->>>>>>> Extracts MoveCalculator and refactors Navigator
    */
   let motions = {
     left:  { level: 0, direction: 'previous' },
@@ -39,7 +25,6 @@ let createNavigator = (opts) => {
     down:  { level: 1, direction: 'next' }
   };
 
-<<<<<<< HEAD
   let toPossibleMotions = (directions) => {
     let reduce  = (result, name) => {
       let motion = motions[name];
@@ -108,105 +93,6 @@ let createNavigator = (opts) => {
    */
   let toState = (motion) => {
     return directions[motion.level] && directions[motion.level][motion.direction];
-=======
-  /**
-   * Sets the currently possible moves and
-   * re-calculates possible directions.
-   *
-   * @param {*[]} moves
-   */
-  let setPossibleMoves = (moves) => {
-    possibleMoves = moves;
-    calculatePossibleDirections();
-  };
-
-  /**
-   * Calculates possible directions based on
-   * possibleMoves
-   */
-  let calculatePossibleDirections = () => {
-    let reduce  = (result, name) => {
-      let motion = motions[name];
-      result[name] = !!(possibleMoves[motion.level] && possibleMoves[motion.level][motion.direction]);
-      return result;
-    };
-
-    possibleDirections = getDirectionNames().reduce(reduce, {});
-  };
-
-  /**
-   * Get motion display names
-   * @returns {string[]} Array of possible motions (left, right, down, up)
-   */
-  let getDirectionNames = () => {
-    return Object.keys(motions);
-  };
-
-  /**
-   * Is direction possible?
-   * @param {string} direction
-   * @returns {boolean}
-   */
-  let isDirectionPossible = (direction) => {
-    return !!possibleDirections[direction];
-  };
-
-  /**
-   * Is state valid?
-   * Tests if state is an array.
-   * @param {*} state
-   * @returns {boolean}
-   */
-  let isValidState = (state) => {
-    return Array.isArray(state);
-  };
-
-  /**
-   * Get level and direction for motion-string
-   * @param {string} direction
-   * @returns {level, direction}
-   */
-  let toLevelAndDirection = (direction) => {
-    return motions[direction];
-  };
-
-  /**
-   * Returns new state from motion
-   * @param {level, direction} motion
-   * @returns {number[]|false}
-   */
-  let toState = (direction) => {
-    let motion = toLevelAndDirection(direction);
-    return possibleMoves[motion.level] && possibleMoves[motion.level][motion.direction];
-  };
-
-  /**
-   * Pushes new state to subject if state is valid.
-   * @param {*} newState
-   */
-  let pushNewState = (newState) => {
-    if(isValidState(newState)) {
-      subject.next(newState);
-    }
-  };
-
-  /**
-   * Calculates new state from direction (left/right/up/down)
-   * and if valid, pushes the new state to the subject.
-   * @param {string} direction
-   */
-  let move = (direction) => {
-    if (!isDirectionPossible(direction)) return;
-    pushNewState(toState(direction));
-  };
-
-  /**
-   * If valid, pushes a new state to the subject.
-   * @param {number[]} newState
-   */
-  let go = (newState) => {
-    pushNewState(newState);
->>>>>>> Extracts MoveCalculator and refactors Navigator
   };
 
   /**
@@ -214,7 +100,6 @@ let createNavigator = (opts) => {
    * @returns {*}
    */
   let asObservable = () => {
-<<<<<<< HEAD
     return Observable.merge(motionObservable, jumpObservable)
       .filter(isValidState)
   };
@@ -256,19 +141,6 @@ let createNavigator = (opts) => {
     motionNames: getMotionNames(),
     next
   };
-=======
-    return subject;
-  };
-
-  return {
-    asObservable,
-    setPossibleMoves,
-    isDirectionPossible,
-    getDirectionNames,
-    move,
-    go
-  }
->>>>>>> Extracts MoveCalculator and refactors Navigator
 };
 
 export default createNavigator;
